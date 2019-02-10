8731640be7b8b51b68759edc0d90b28c24eda904
Kevin Zhou <kevinz94@live.com>
2019-02-10 18:09:08 -0500

    get order data from firebase and display it on Order Page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,13 +1,38 @@
 import React from "react";
 import "./Order.css";
 
-const order = props => (
-  <div className="Order">
-    <p>Ingredients: Salad (1) </p>
-    <p>
-      Price: <strong>USD 5.45</strong>
-    </p>
-  </div>
-);
+const order = props => {
+  const fillings = [];
+
+  for (let fillingName in props.fillings) {
+    fillings.push({ name: fillingName, amount: props.fillings[fillingName] });
+  }
+
+  const fillingOutput = fillings.map(ig => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px"
+        }}
+        key={ig.name}
+      >
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
+  return (
+    <div className="Order">
+      <p>Ingredients: {fillingOutput} </p>
+      <p>
+        Price: <strong>USD {props.price}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default order;
