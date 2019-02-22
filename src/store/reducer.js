88130d9e1582c11a4f3035521f463ee323f4e6f0
Kevin Zhou <kevinz94@live.com>
2019-02-22 18:08:33 -0500

    initialized react-redux component

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  fillings: null,
+  totalPrice: 2.0
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
